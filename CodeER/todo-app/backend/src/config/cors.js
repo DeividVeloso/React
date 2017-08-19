221e60c975d0e031820565b0abdd5907959a7abb
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-08-19 15:31:14 -0300

    add cors in API

---
+++ CodeER/todo-app/backend/src/config/cors.js
@@ -0,0 +1,9 @@
+ //Requisição 
+ //resposta
+ //next(chain) - Padrão chain of reponsability é o chain que diz se ele vai para o próximo filtro ou proximo middleware.
+ module.exports = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Origin', 'Origin, X-Request-With, Content-Type, Accept');
+    next()
+ }
--- CodeER/todo-app/backend/src/config/server.js
+++ CodeER/todo-app/backend/src/config/server.js
@@ -6,13 +6,17 @@ const bodyParser = require('body-parser');
 const express = require('express');
 //Startando o servidor
 const server = express();
+//Habilitando o cors
+const allowCors = require('./cors');
 
 //Adicionando middleware para o servidor
 //Ele desserializa e transforma em object Javascript
 //Esse serve para quando for dados enviado por post do formulário
 server.use(bodyParser.urlencoded({ extends: true}));
 //Esse serve para desserializar JSON para  object Javascript
-server.use(bodyParser.json())
+server.use(bodyParser.json());
+server.use(allowCors);
+
 
 server.listen(port, function(){
     console.log(`BACKEND is running on port ${port}.`)
