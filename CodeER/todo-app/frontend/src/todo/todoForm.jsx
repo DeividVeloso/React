c541e5731f524a1a70e154b5e1a50a4ccd129b3a
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-09-22 10:02:59 -0300

    shortcut

--- CodeER/todo-app/frontend/src/todo/todoForm.jsx
+++ CodeER/todo-app/frontend/src/todo/todoForm.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import Grid from "../template/grid";
 import Button from "../template/iconButton";
 
-export default props => (
-  <div role="form" className="todoForm">
-    <Grid cols="12 9 10">
-      <input
-        type="text"
-        id="description"
-        className="form-control"
-        placeholder="Adicione uma tarefa"
-        value={props.description}
-        onChange={props.handleChange}
-      />
-    </Grid>
-    <Grid cols="12 3 2">
-      <Button style="primary" icon="plus" onClick={props.handleAdd} />
-      <Button style="info" icon="search" onClick={props.handleSearch} />
-      <Button style='default' icon='close' onClick={ props.handleClear}/>
+export default props => {
+  const keyHandler = e => {
+    if (e.key === "Enter") {
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
+    } else if (e.key === "Escape") {
+      props.handleClear();
+    }
+  };
 
-    </Grid>
-  </div>
-);
+  return (
+    <div role="form" className="todoForm">
+      <Grid cols="12 9 10">
+        <input
+          type="text"
+          id="description"
+          className="form-control"
+          placeholder="Adicione uma tarefa"
+          value={props.description}
+          onKeyUp={keyHandler}
+          onChange={props.handleChange}
+        />
+      </Grid>
+      <Grid cols="12 3 2">
+        <Button style="primary" icon="plus" onClick={props.handleAdd} />
+        <Button style="info" icon="search" onClick={props.handleSearch} />
+        <Button style='default' icon='close' onClick={props.handleClear} />
+      </Grid>
+    </div>
+  );
+}
