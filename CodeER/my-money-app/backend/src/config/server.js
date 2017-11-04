8a0cc4ee21d45a330863610436acc76327491c2d
Deivid Veloso <veloso.deivid@gmail.com>
2017-11-04 12:13:25 -0200

    add cors

--- CodeER/my-money-app/backend/src/config/server.js
+++ CodeER/my-money-app/backend/src/config/server.js
@@ -5,8 +5,11 @@ const express = require("express"); // CommonJs retornar singleton
 
 const server = express(); // Sempre retorna uma nova instancia.
 
+const allowCors = require("./cors");
+
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`);
