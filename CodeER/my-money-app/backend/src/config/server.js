6af22e7df50d99a587a6c2c85d3747dddcdba0d1
Deivid Veloso <veloso.deivid@gmail.com>
2017-11-04 12:23:04 -0200

    add pagination

--- CodeER/my-money-app/backend/src/config/server.js
+++ CodeER/my-money-app/backend/src/config/server.js
@@ -2,14 +2,15 @@ const port = 3003;
 //Adicionando os middlewares
 const bodyParser = require("body-parser");
 const express = require("express"); // CommonJs retornar singleton
-
 const server = express(); // Sempre retorna uma nova instancia.
-
 const allowCors = require("./cors");
+
+const queryParser = require("express-query-int");
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
+server.use(queryParser());
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}.`);
