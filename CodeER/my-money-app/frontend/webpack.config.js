8cbf40822bfcc822ae2fe4468a83ab6f2cd4d7a6
Deivid Veloso <veloso.deivid@gmail.com>
2017-11-11 13:58:46 -0200

    add entry point, i have a problem in webpack to generate files, my loader font was write wrong 'text' agaist 'test' I chage to test and fix

--- CodeER/my-money-app/frontend/webpack.config.js
+++ CodeER/my-money-app/frontend/webpack.config.js
@@ -14,9 +14,9 @@ module.exports = {
   resolve: {
     extensions: ["", ".js", ".jsx"],
     alias: {
-      modules: __dirname + "/node_modules/",
-      jquery: "modules/adim-lte/plugins/jQuery/jquery-2.2.3.min.js",
-      bootstrap: "modules/adim-lte/bootstrap/js/bootstrap.js"
+      modules: __dirname + "/node_modules",
+      jquery: "modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js",
+      bootstrap: "modules/admin-lte/bootstrap/js/bootstrap.js"
     }
   },
   plugins: [
@@ -43,7 +43,7 @@ module.exports = {
         loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       },
       {
-        text: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
         loader: "file"
       }
     ]
