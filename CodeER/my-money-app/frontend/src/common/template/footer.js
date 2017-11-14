1ed15d4da54085adc82dd1833ac9572f6f4c53a5
Deivid Veloso <veloso.deivid@gmail.com>
2017-11-14 19:21:47 -0200

    add footer and content

---
+++ CodeER/my-money-app/frontend/src/common/template/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2017
+      <a href="http://negociodireto.com" target="_blank">
+        {" "}
+        Negocio
+      </a>
+    </strong>
+  </footer>
+);
