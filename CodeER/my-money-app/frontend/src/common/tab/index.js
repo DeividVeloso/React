0bf7287e703d67e4be67912c8ee6bf217f6bcc87
Deivid Veloso <veloso.deivid@gmail.com>
2017-12-02 13:57:04 -0200

    adicionado tabs

--- CodeER/my-money-app/frontend/src/common/tab/index.js
+++ CodeER/my-money-app/frontend/src/common/tab/index.js
@@ -2,3 +2,4 @@ export { default as Tabs } from "./tabs";
 export { default as TabsHeader } from "./tabs-header";
 export { default as TabsContent } from "./tabs-content";
 export { default as TabHeader } from "./tab-header";
+export { default as TabContent } from "./tab-content";
--- CodeER/my-money-app/frontend/src/components/billing-cycle/index.js
+++ CodeER/my-money-app/frontend/src/components/billing-cycle/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { selectTab } from "../../store/actions/tab-action";
 import Content from "../../common/template/content";
 import ContentHeader from "../../common/template/content-header";
-import { Tabs, TabsHeader, TabsContent, TabHeader } from "../../common/tab/";
+import {
+  Tabs,
+  TabsHeader,
+  TabsContent,
+  TabHeader,
+  TabContent
+} from "../../common/tab/";
 
-export default class BillingCycle extends Component {
+class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab("tabList");
+  }
   render() {
     return (
       <div>
@@ -16,10 +28,27 @@ export default class BillingCycle extends Component {
               <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
               <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
             </TabsHeader>
-            <TabsContent />
+            <TabsContent>
+              <TabContent id="tabList">
+                <h1>Lista</h1>
+              </TabContent>
+              <TabContent id="tabCreate">
+                <h1>Create</h1>
+              </TabContent>
+              <TabContent id="tabUpdate">
+                <h1>Update</h1>
+              </TabContent>
+              <TabContent id="tabDelete">
+                <h1>Delete</h1>
+              </TabContent>
+            </TabsContent>
           </Tabs>
         </Content>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ selectTab }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycle);
