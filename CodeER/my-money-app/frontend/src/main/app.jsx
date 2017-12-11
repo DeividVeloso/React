7402aa6575b0848be63088d7fc9ff1c305e4b192
Deivid Veloso <veloso.deivid@gmail.com>
2017-12-11 19:36:21 -0200

    fix package.json

--- CodeER/my-money-app/frontend/src/main/app.jsx
+++ CodeER/my-money-app/frontend/src/main/app.jsx
@@ -1,15 +1,14 @@
-import "../common/template/dependencies";
-import React from "react";
-import Header from "../common/template/header";
-import SideBar from "../common/template/sideBar";
-import Footer from "../common/template/footer";
-import Messages from "../common/message/";
+import React from 'react';
+import Header from '../common/template/header';
+import SideBar from '../common/template/sideBar';
+import Footer from '../common/template/footer';
+import Messages from '../common/message/';
+import '../common/template/dependencies';
+import Routes from './routes';
 
-import Routes from "./routes";
-
-const a = 1
-const b = 2
-a = 4
+const a = 1;
+const b = 2;
+a = 4;
 
 export default props => (
   <div className="wrapper">
