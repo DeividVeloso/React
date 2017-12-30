c6fa733aabad51352a0034de178b7fa30204f46e
Deivid Veloso <veloso.deivid@gmail.com>
2017-12-30 17:00:45 -0200

    add remove and clone credits

--- CodeER/my-money-app/frontend/src/components/billing-cycle/components/credit-list/index.js
+++ CodeER/my-money-app/frontend/src/components/billing-cycle/components/credit-list/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Field, arrayInsert, arrayRemove } from 'redux-form';
 import Grid from '../../../../common/layout/grid';
 import Input from '../../../../common/input';
 
 class CreditList extends Component {
-  add(index) {
-    console.log(index);
+  add(index, item = {}) {
+    if (!this.props.readOnly) {
+      console.log('Entrei', this.props.arrayInsert('billigCycleForm', 'credits', index, item));
+      this.props.arrayInsert('billingCycleForm', 'credits', index, item);
+    }
+  }
+
+  remove(index) {
+    if (!this.props.readOnly && this.props.list.length > 1) {
+      this.props.arrayRemove('billingCycleForm', 'credits', index);
+    }
   }
   renderRows() {
     const list = this.props.list || [];
@@ -31,6 +42,16 @@ class CreditList extends Component {
           <button type="button" className="btn btn-success" onClick={() => this.add(index + 1)}>
             <i className="fa fa-plus" />
           </button>
+          <button
+            type="button"
+            className="btn btn-warning"
+            onClick={() => this.add(index + 1, item)}
+          >
+            <i className="fa fa-clone" />
+          </button>
+          <button type="button" className="btn btn-danger" onClick={() => this.remove(index)}>
+            <i className="fa fa-trash" />
+          </button>
         </td>
       </tr>
     ));
@@ -46,7 +67,7 @@ class CreditList extends Component {
               <tr>
                 <th>Nome</th>
                 <th>Valor</th>
-                <th>Ações</th>
+                <th className="table-actions">Ações</th>
               </tr>
             </thead>
             <tbody>{this.renderRows()}</tbody>
@@ -56,4 +77,7 @@ class CreditList extends Component {
     );
   }
 }
-export default CreditList;
+
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch);
+
+export default connect(null, mapDispatchToProps)(CreditList);
